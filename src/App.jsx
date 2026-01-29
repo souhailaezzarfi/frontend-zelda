8f803d7dfb33fe5b0e53ccb38ef92ddb55d35d40
Souhaila Ezzarfi <souhaila_ezzarfi@iescarlesvallbona.cat>
2026-01-29 15:56:04 +0100

    Primer commit: proyecto React con Bootstrap listo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Bootstrap funciona 🎉</h1>
+      <button className="btn btn-success">Botón</button>
+    </div>
+  )
+}
+
+export default App
